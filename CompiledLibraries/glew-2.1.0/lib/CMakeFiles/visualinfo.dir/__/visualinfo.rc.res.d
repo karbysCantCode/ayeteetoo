20e3130b387f4a09179db7c977b5cc668266555e
karbysCantCode <realgeekscorp@gmail.com>
2025-02-28 17:29:54 +1300

    Add project files.

---
+++ CompiledLibraries/glew-2.1.0/lib/CMakeFiles/visualinfo.dir/__/visualinfo.rc.res.d
@@ -0,0 +1,19 @@
+D:\\C++\ programs\\ayeteetoo\\CompiledLibraries\\glew-2.1.0\\lib\\CMakeFiles\\visualinfo.dir\\__\\visualinfo.rc.res: \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\shared\\SpecStrings.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\shared\\driverspecs.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\shared\\sdkddkver.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\shared\\winapifamily.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\shared\\winpackagefamily.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\commctrl.rh \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\dde.rh \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\dlgs.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\verrsrc.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\windows.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\winnt.rh \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\winresrc.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\winuser.rh \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\\\include\\10.0.22621.0\\\\um\\winver.h \
+C:\\Program\ Files\ (x86)\\Windows\ Kits\\10\\include\\10.0.22621.0\\shared\\sdv_driverspecs.h \
+C:\\Program\ Files\\Microsoft\ Visual\ Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.43.34808\\include\\concurrencysal.h \
+C:\\Program\ Files\\Microsoft\ Visual\ Studio\\2022\\Community\\VC\\Tools\\MSVC\\14.43.34808\\include\\sal.h \
+
---
+++ Source/Source/Application.cpp
@@ -0,0 +1,10 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+
+
+int main() {
+	std::cout << "Hello World!" << std::endl;
+}
+
